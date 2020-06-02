79a602ecdde742c205c7b3c2e1f6d05fd96f4fe1
Bo Zhang <dlzbo@cn.ibm.com>
2020-06-02 14:34:01 +0800

    add enable eureka

--- src/main/java/com/smc/PriceApplication.java
+++ src/main/java/com/smc/PriceApplication.java
@@ -2,6 +2,7 @@ package com.smc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author BoZhang
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version date：May 12, 2020 7:52:18 PM
 */
 @SpringBootApplication
+@EnableEurekaClient
 public class PriceApplication {
 
 	public static void main(String[] args) {
